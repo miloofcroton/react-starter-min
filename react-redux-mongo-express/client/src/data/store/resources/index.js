d9b59990a3520c8709a4770c8d49687c6a067cbf
Jack <ajackofsometrades@gmail.com>
2019-01-18 15:01:06 -0800

    adding users to reducer (could separate users into sessions, but nah)

--- react-redux-mongo-express/client/src/data/store/resources/index.js
+++ react-redux-mongo-express/client/src/data/store/resources/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import usersReducer from './users/reducers';
 import itemsReducer from './items/reducers';
 
 export const combinedReducers = combineReducers({
+  users: usersReducer,
   items: itemsReducer
 });
