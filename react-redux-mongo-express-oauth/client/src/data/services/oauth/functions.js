a88ecf57cac3d8a5cbe21c6dea2ae803e05441f1
Jack <ajackofsometrades@gmail.com>
2019-01-21 18:43:27 -0800

    initial functions for oauth connection

--- react-redux-mongo-express-oauth/client/src/data/services/oauth/functions.js
+++ react-redux-mongo-express-oauth/client/src/data/services/oauth/functions.js
@@ -1,16 +1,8 @@
-import { WebAuth, Management } from 'auth0-js';
+import { Management } from 'auth0-js';
+import { auth0 } from './config';
 import store from '../../store';
 import { updateAuthToken, updateSessionProfile } from '../../store/resources/oauth/actions';
 import { getAuthToken } from '../../store/resources/oauth/selectors';
-
-const auth0 = new WebAuth({
-  domain: process.env.AUTH0_DOMAIN,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  redirectUri: process.env.AUTH0_REDIRECT,
-  audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
-  responseType: 'token',
-  scope: 'openid profile create:current_user_metadata read:current_user update:current_user_metadata'
-});
 
 export const signup = (email, password, name, photo) => {
   return new Promise((resolve, reject) => {
