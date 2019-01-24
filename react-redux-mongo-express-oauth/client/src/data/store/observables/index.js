210e17b2af7bcc0a7f138d857ac261a7096c1f09
Jack <ajackofsometrades@gmail.com>
2019-01-24 01:00:14 -0800

    fixed the double epic issue
    wow... I feel very embarrased

--- react-redux-mongo-express-oauth/client/src/data/store/observables/index.js
+++ react-redux-mongo-express-oauth/client/src/data/store/observables/index.js
@@ -1,7 +1,7 @@
 import { combineEpics } from 'redux-observable';
 import itemsEpics from './items';
 import thingsEpics from './things';
-import sessionsEpics from './items';
+import sessionsEpics from './sessions';
 
 export const rootEpic = combineEpics(
   ...itemsEpics,
