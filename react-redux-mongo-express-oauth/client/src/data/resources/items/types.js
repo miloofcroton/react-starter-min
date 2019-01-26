0f04c89b45d45f3d1df7b85572957e72ef40d9e6
Jack <ajackofsometrades@gmail.com>
2019-01-26 05:33:21 -0800

    making naming identical to redux-form and connected router

--- react-redux-mongo-express-oauth/client/src/data/resources/items/types.js
+++ react-redux-mongo-express-oauth/client/src/data/resources/items/types.js
@@ -1,8 +1,8 @@
-export const FETCH_ONE_START = 'items/FETCH_ONE_START';
-export const FETCH_ONE_DONE = 'items/FETCH_ONE_DONE';
+export const FETCH_ONE_START = '@@items/FETCH_ONE_START';
+export const FETCH_ONE_DONE = '@@items/FETCH_ONE_DONE';
 
-export const FETCH_LIST_START = 'items/FETCH_LIST_START';
-export const FETCH_LIST_DONE = 'items/FETCH_LIST_DONE';
+export const FETCH_LIST_START = '@@items/FETCH_LIST_START';
+export const FETCH_LIST_DONE = '@@items/FETCH_LIST_DONE';
 
-export const POST_ONE_START = 'items/POST_ONE_START';
-export const POST_ONE_DONE = 'items/POST_ONE_DONE';
+export const POST_ONE_START = '@@items/POST_ONE_START';
+export const POST_ONE_DONE = '@@items/POST_ONE_DONE';
--- react-redux-mongo-express-oauth/client/src/data/resources/things/types.js
+++ react-redux-mongo-express-oauth/client/src/data/resources/things/types.js
@@ -1,8 +1,8 @@
-export const FETCH_ONE_START = 'things/FETCH_ONE_START';
-export const FETCH_ONE_DONE = 'things/FETCH_ONE_DONE';
+export const FETCH_ONE_START = '@@things/FETCH_ONE_START';
+export const FETCH_ONE_DONE = '@@things/FETCH_ONE_DONE';
 
-export const FETCH_LIST_START = 'things/FETCH_LIST_START';
-export const FETCH_LIST_DONE = 'things/FETCH_LIST_DONE';
+export const FETCH_LIST_START = '@@things/FETCH_LIST_START';
+export const FETCH_LIST_DONE = '@@things/FETCH_LIST_DONE';
 
-export const POST_ONE_START = 'things/POST_ONE_START';
-export const POST_ONE_DONE = 'things/POST_ONE_DONE';
+export const POST_ONE_START = '@@things/POST_ONE_START';
+export const POST_ONE_DONE = '@@things/POST_ONE_DONE';
