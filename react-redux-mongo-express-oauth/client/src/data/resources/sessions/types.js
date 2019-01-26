24360593faba7331532edc38f580048ec38ba256
Jack <ajackofsometrades@gmail.com>
2019-01-26 05:11:20 -0800

    spacing

--- react-redux-mongo-express-oauth/client/src/data/resources/sessions/types.js
+++ react-redux-mongo-express-oauth/client/src/data/resources/sessions/types.js
@@ -3,6 +3,7 @@ export const SESSION_UPDATE_TOKEN = 'sessions/UPDATE_TOKEN';
 export const SESSION_UPDATE_PROFILE = 'sessions/UPDATE_PROFILE';
 
 export const SIGN_IN_START = 'sessions/SIGN_IN_START';
+
 export const SIGN_IN_DONE = 'sessions/SIGN_IN_DONE';
 
 export const SIGN_UP = 'sessions/SIGN_UP';
