c274333afc0f936cec47717f8afecef98591a964
Jack <ajackofsometrades@gmail.com>
2019-01-27 10:59:42 -0800

    removed unnecessary purecomponents
    thanks to fetching being done on route-based epics, pure functional react, he were come!

--- react-redux-mongo-express-oauth/client/src/views/content/home/Auth.js
+++ react-redux-mongo-express-oauth/client/src/views/content/home/Auth.js
@@ -1,25 +1,20 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { signout } from '../../../data/resources/sessions/actions';
 import { getSessionToken } from '../../../data/resources/sessions/selectors';
 
-class Auth extends PureComponent {
+const Auth = ({ session, signout }) => {
 
-  render() {
-
-    const { session, signout } = this.props;
-
-    return (
-      <div>
-        {
-          (!!session && session.user !== null)
-            ? <button onClick={signout}>Sign Out</button>
-            : <p>Sign up or sign in!</p>
-        }
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {
+        (!!session && session.user !== null)
+          ? <button onClick={signout}>Sign Out</button>
+          : <p>Sign up or sign in!</p>
+      }
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   session: getSessionToken(state),
--- react-redux-mongo-express-oauth/client/src/views/lib/loading/Fallback.js
+++ react-redux-mongo-express-oauth/client/src/views/lib/loading/Fallback.js
@@ -1,6 +1,5 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-// note: this will cause race conditions if child component affects the loading state on mount
 export const Fallback = LoadingComponent => Component => {
   return function LoadingFallbackComponent(props) {
     if (props.loading) return (
@@ -8,22 +7,5 @@ export const Fallback = LoadingComponent => Component => {
     );
 
     return <Component {...props} />;
-  };
-};
-
-// useful if you don't want to use a higher order fetching component
-export const FallbackWithFetch = LoadingComponent => Component => {
-  return class LoadingFallbackComponent extends PureComponent {
-    componentDidMount() {
-      this.props.fetch();
-    }
-
-    render() {
-      return (
-        this.props.loading
-          ? <LoadingComponent {...this.props} />
-          : <Component {...this.props } />
-      );
-    }
   };
 };
